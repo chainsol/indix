6af338bbd5d0941b736b421e529041bb797cb43d
Matthew Bentley <matthew.t.bentley@gmail.com>
2014-01-19 05:33:11 -0800

    Added some documentation, added error handling

--- indix.py
+++ indix.py
@@ -4,6 +4,25 @@ import os
 
 sortBy_legal = ["RELEVANCE", "PRICE_LOW_TO_HIGH", "PRICE_HIGH_TO_LOW", "MOST_RECENT", ""]
 
+class indixException(Exception):
+    pass
+
+class indixRestException(indixException): #TODO: use this
+    """ This should do something with error codes from indix. Maybe.
+
+    :param int status: the error code
+    :param str uri: the url
+    :param str msg: the message, if one exists
+    """
+
+    def __init__(self, status, uri, msg=""):
+        self.uri=uri
+        self.status=status
+        self.msg=msg
+
+    def __str__(self):
+        return "ERROR %s: %s (%s)" % (self.status, self.msg, self.uri)
+
 def find_credentials():
     """Look in the current environment for indix account creds"""
     try:
@@ -40,51 +59,119 @@ class IndixRestClient(object):
         if not app_id or not app_key:
             app_id, app_key = find_credentials()
             if not app_id or not app_key:
-                print "You need a app_id and app_key to work, you idiot."
+                raise indixException("""You need app_id and app_key when you initialize stuff,
+                or add environment variables (see readme)""")
 
         self.base = base
         self.app_id, self.app_key = app_id, app_key
         self.version_uri = "%s/%s" % (base, version)
 
     def brands(self, query=None):
+        """
+        :param query: the brand you want to search for
+        """
+
         response = make_request(self.version_uri, "brands", query=query,
                                 app_id=self.app_id, app_key=self.app_key)
-        return response
+        
+        if response.ok:
+            return response
+        else:
+            raise indixRestException(response.status_code, response.url, response.reason)
     
     def stores(self, query=None):
+        """
+        :param query: the stores you want to search for
+        """
+
         response = make_request(self.version_uri, "stores", query=query,
                                 app_id=self.app_id, app_key=self.app_key)
-        return response
+        
+        if response.ok:
+            return response
+        else:
+            raise indixRestException(response.status_code, response.url, response.reason)
 
-    def catagories(self):
+    def categories(self):
+        """
+        Takes not params.  Returns json for all possible catagories
+        """
+
         response = make_request(self.version_uri, "categories",
                                 app_id=self.app_id, app_key=self.app_key)
-        return response
+
+        if response.ok:
+            return response
+        else:
+            raise indixRestException(response.status_code, response.url, response.reason)
 
     def products(self, pageNumber=1, query="", storeId="", brandId="", categoryId="",
                  startPrice="", endPrice="", sortBy="", priceHistoryAvail=False):
-        if sortBy not in sortBy_legal:
-            print "sortBy must be a legal value, see the source for more"
-            return -1
+        """
+        :param int pageNumber: page number to get
+        :param query: product to search for
+        :param storeId: storeId, form indix.stores
+        :param brandId: brandId, from indix.brands
+        :param categoryId: categoryId, from indix.categories
+        :param startPrice: low price
+        :param endPrice: high price
+        :sortBy: must be one of: "RELEVANCE", "PRICE_LOW_TO_HIGH", "PRICE_HIGH_TO_LOW", "MOST_RECENT"
+        or blank
+        :priceHistoryAvail bool: if True, will only return products with price history available
+        Returns: json for 10 products, dependent on the page you choose
+        """
+
         response = make_request(self.version_uri, "products", pageNumber=pageNumber, query=query,
                                 storeId=storeId, brandId=brandId, categoryId=categoryId,
                                 startPrice=startPrice, endPrice=endPrice, sortBy=sortBy,
                                 priceHisotryAvail=priceHistoryAvail,
                                 app_id=self.app_id, app_key=self.app_key)
-        return response
+        if response.ok:
+            return response
+        else:
+            raise indixRestException(response.status_code, response.url, response.reason)
 
     def productById(self, id=None, pageNumber=1):
+        """
+        :param id: productId from indix.products
+        :param pageNumber: page number
+        Returns: JSON for correct page of that product
+        """
+
         response = make_request(self.version_uri, "products/%s" % id, pageNumber=pageNumber,
                                 app_id=self.app_id, app_key=self.app_key)
-        return response
+        
+        if response.ok:
+            return response
+        else:
+            raise indixRestException(response.status_code, response.url, response.reason)
 
     def pricesById(self, id=None):
+        """
+        :param id: productId from indix.products
+        Returns json for price history for an item
+        """
+
         response = make_request(self.version_uri, "products/%s/prices" % id,
                                 app_id=self.app_id, app_key=self.app_key)
-        return response
+        
+        if response.ok:
+            return response
+        else:
+            raise indixRestException(response.status_code, response.url, response.reason)
 
     def raw(self, endpoint=None, **kwargs):
+        """
+        :param endpoint: the url to all to the end of api.indix.com/api/beta/
+        :param **kwargs: dictionary of extra things to add to url as a GET request
+        Returns: json
+        """
+
         response = make_request(self.version_uri, endpoint,
                                 app_id=self.app_id, app_key=self.app_key,
                                 **kwargs)
-        return response
+        
+        if response.ok:
+            return response
+        else:
+            raise indixRestException(response.status_code, response.url, response.reason)
